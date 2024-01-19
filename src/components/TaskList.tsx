583182d1f4137ee9010daa4bb7d3102f0cd4f7fa
Robin K <Robin.kron@lerniastudent.se>
2024-01-19 09:49:08 +0100

    tasklist that render tasks done, instant uppdate if collection changes

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React from "react";
+import Task from "./Task";
+import { onSnapshot } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { taskCollectionRef } from "../app/firebase/collections/firestore.collections";
+import { taskData } from "@/lib/types";
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState<taskData[]>();
+
+  useEffect(() => {
+    const unsub = onSnapshot(taskCollectionRef, (docCollection) => {
+      setTasks(
+        docCollection.docs.map((doc) => ({
+          id: doc.id,
+          category: doc.data().category,
+          description: doc.data().description,
+        }))
+      );
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  return (
+    <ul className="max-w-xl flex flex-col h-96 mx-auto mt-10 bg-lightPink">
+      {tasks?.map((task) => (
+        <li key={task.id}>
+          <Task
+            id={task.id}
+            category={task.category}
+            description={task.description}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TaskList;
