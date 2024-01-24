a4f21f5951347582887367e1dca373ea1c8905e9
Robin K <Robin.kron@lerniastudent.se>
2024-01-24 10:03:56 +0100

    Added login/logout buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
+import Login from "./auth/Login";
+import Logout from "./auth/Logout";
+import { getServerSession } from "next-auth";
 
-const Header = () => {
+const Header = async () => {
+  const session = await getServerSession();;
+
   return (
-
-  <div className="bg-darkRed flex justify-between min-h-20 shadow-md">
-    <h1 className="text-6xl m-3 shadow-sm">TP</h1>
-    <div className='flex flex-col-reverse '>"login"</div>
-
-  </div>
-)}
+    <div className="bg-darkRed flex justify-between min-h-20 shadow-md">
+      <h1 className="text-6xl m-3 shadow-sm">TP</h1>
+      <div className="flex flex-col-reverse ">
+        {!session ? <Login /> : <Logout />}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
