c807eb6337c16733b15270ffeaf852d952857148
Robin K <Robin.kron@lerniastudent.se>
2024-01-25 15:23:06 +0100

    deployfix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const authOptions = {
   
   providers: [
     GoogleProvider({
@@ -11,5 +11,5 @@ export const authOptions = {
   ],
 }
 
-export const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST } 
