bb55b5e812d6bea588368a92a7178719d329ce03
Robin K <Robin.kron@lerniastudent.se>
2024-01-25 11:02:41 +0100

    some style adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} text-lightPink h-screen`}>
         <AuthProvider session={session}>
           <Header />
           {!session ? (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import { useSession } from 'next-auth/react'
 import TaskList from "@/components/TaskList";
 
@@ -8,10 +7,10 @@ export default function Home() {
   const session = useSession();
 
   return (
-    <div className="bg-black h-screen">
+    <>
       <h1 className="text-center text-5xl font-extrabold">{session?.data?.user?.name}</h1>
       <h1 className="text-center text-5xl font-extrabold">{session?.data?.user?.email}</h1>
       <TaskList/>
-    </div>
+    </>
   );
 }
